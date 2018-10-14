3f56f4a99c16bd62737505b3ab2893de8fa5e62e
Peter Johennecken <peter.johennecken@googlemail.com>
2018-10-14 21:54:54 +0200

    fixed update_links.sh
    Signed-off-by: Peter Johennecken <peter.johennecken@googlemail.com>

--- update_links.sh
+++ update_links.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/env bash
 
 if [[ ! $XDG_CONFIG_HOME ]]; then
-    __CONFIG__=$HOME/.config/
+    __CONFIG__=$HOME/.config
 else
     __CONFIG__=$XDG_CONFIG_HOME
 fi
 
-ln -s $(pwd)/git/ $__CONFIG__/git/
-ln .s $(pwd)/i3/ $__CONFIG__/i3/
-ln .s $(pwd)/i3lock/ $__CONFIG__/i3lock/
-ln .s $(pwd)/i3status/ $__CONFIG__/i3status/
-ln .s $(pwd)/nvim/ $__CONFIG__/nvim/
+ln -s $(pwd)/git/ $__CONFIG__/git
+ln -s $(pwd)/i3/ $__CONFIG__/i3
+ln -s $(pwd)/i3lock/ $__CONFIG__/i3lock
+ln -s $(pwd)/i3status/ $__CONFIG__/i3status
+ln -s $(pwd)/nvim/ $__CONFIG__/nvim
